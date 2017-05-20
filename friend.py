299f1889b26c59755d38c23e5b1133a0de00043b
biog12 <biog12@qq.com>
2017-05-20 19:14:19 +0800

    Update friend.py

--- friend.py
+++ friend.py
@@ -1,29 +1,24 @@
-import itchat #itchat包
+import itchat
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 plt.rcParams['font.sans-serif']=['SimHei'] #用来正常显示中文标签
 plt.rcParams['axes.unicode_minus']=False #用来正常显示负号
-itchat.login()
+itchat.login() #微信登录
 #获取好友列表
 friends = itchat.get_friends(update=True)[0:]
 i=0
-a=[]
+a=[]#好友城市列表
 for i in range(len(friends)):
     a.append(friends[i].City)
     i+=1
-b=set(a)
-print(len(friends))
-c=[]
+#
+b=set(a)#城市不重复集合
+#城市不重复集合出现的次数统计
 d=[]
-
 for item in b:
-     c.append(item)
      d.append(a.count(item))
-
-#print(c)
-#print(c)
-#print(d)
+#柱状图
 plt.bar(range(len(d)),d,tick_label=c,width=0.5,align="center")
 plt.xticks(rotation=90)
 plt.title("好友地理位置统计")
